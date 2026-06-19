fcc2da4b616741d50c1f8d62404b58c69cac7a77
vishal <vvishalkkumar19@gmail.com>
2026-06-19 10:57:08 +0530

    UI: disable native pinch zoom on iOS Safari

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,11 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 
+// Disable iOS Safari native pinch-zoom
+document.addEventListener('gesturestart', function(e) {
+  e.preventDefault();
+});
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
